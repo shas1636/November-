bd6a53f9934064bdb3f6eb74306017a7356ff76d
Ankit Kumar <ankit@privatedeal.com>
2022-11-07 17:26:26 +0530

    first file to move in git repo

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/1a/50510f27855e001d19fbc60ee49804f0
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ajay Kumar
+ *
+ */
+module sampleProject {
+}
---
+++ sampleProject/src/samplePackage/SampleClass.java
@@ -0,0 +1,10 @@
+package samplePackage;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
